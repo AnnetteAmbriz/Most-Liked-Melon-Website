ac527529971e9e7c2f2c6c56fec81eebcf9ea77a
Annette Ambriz <annetteambriz@yahoo.com>
2018-03-29 11:05:20 -0700

    All features complete except the optional task

---
+++ melons.py
@@ -0,0 +1,23 @@
+# Getting our list of MOST LOVED MELONS
+MOST_LOVED_MELONS = {
+    'cren': {
+        'img': 'http://www.rareseeds.com/assets/1/14/DimRegular/crenshaw.jpg',
+        'name': 'Crenshaw',
+        'num_loves': 584,
+    },
+    'jubi': {
+        'img': 'http://www.rareseeds.com/assets/1/14/DimThumbnail/Jubilee-Watermelon-web.jpg',
+        'name': 'Jubilee Watermelon',
+        'num_loves': 601,
+    },
+    'sugb': {
+        'img': 'http://www.rareseeds.com/assets/1/14/DimThumbnail/Sugar-Baby-Watermelon-web.jpg',
+        'name': 'Sugar Baby Watermelon',
+        'num_loves': 587,
+    },
+    'texb': {
+        'img': 'http://www.rareseeds.com/assets/1/14/DimThumbnail/Texas-Golden-2-Watermelon-web.jpg',
+        'name': 'Texas Golden Watermelon',
+        'num_loves': 598,
+    },
+}
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, redirect, request, render_template, session
 from flask_debugtoolbar import DebugToolbarExtension
 from jinja2 import StrictUndefined
+from melons import MOST_LOVED_MELONS
 
 
 app = Flask(__name__)
@@ -8,34 +9,33 @@ app.jinja_env.undefined = StrictUndefined
 app.jinja_env.auto_reload = True
 
 # Required to use Flask sessions and the debug toolbar
-app.secret_key = "ABC"
+app.secret_key = "SECRETKEY"
 
-# Getting our list of MOST LOVED MELONS
-MOST_LOVED_MELONS = {
-    'cren': {
-        'img': 'http://www.rareseeds.com/assets/1/14/DimRegular/crenshaw.jpg',
-        'name': 'Crenshaw',
-        'num_loves': 584,
-    },
-    'jubi': {
-        'img': 'http://www.rareseeds.com/assets/1/14/DimThumbnail/Jubilee-Watermelon-web.jpg',
-        'name': 'Jubilee Watermelon',
-        'num_loves': 601,
-    },
-    'sugb': {
-        'img': 'http://www.rareseeds.com/assets/1/14/DimThumbnail/Sugar-Baby-Watermelon-web.jpg',
-        'name': 'Sugar Baby Watermelon',
-        'num_loves': 587,
-    },
-    'texb': {
-        'img': 'http://www.rareseeds.com/assets/1/14/DimThumbnail/Texas-Golden-2-Watermelon-web.jpg',
-        'name': 'Texas Golden Watermelon',
-        'num_loves': 598,
-    },
-}
 
-# YOUR ROUTES GO HERE
+@app.route("/")
+def index():
+    """Return homepage."""
+    if 'username' in session:
+        return redirect("/top-melons")
+    return render_template("homepage.html")
 
+@app.route("/get-name")
+def get_person_name():
+    """Get name from form on homepage"""
+    username = request.args.get("name")
+    session['username'] = username
+    return redirect("/top-melons")
+
+
+@app.route("/top-melons")
+def display_top_melons():
+    """
+    Check if name in session if so, display top melons if not
+    redirect to "/"
+    """
+    if 'username' in session:
+        return render_template("top-melons.html", melon_dic=MOST_LOVED_MELONS)
+    return redirect("/")
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
@@ -43,6 +43,6 @@ if __name__ == "__main__":
     app.debug = True
 
     # Use the DebugToolbar
-    DebugToolbarExtension(app)
+    #DebugToolbarExtension(app)
 
     app.run(host="0.0.0.0")
